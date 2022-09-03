135d66c8c649396b2cd09ee46800eac0c5aacbf1
Renat Skosarev <skosarrrev@gmail.com>
2022-09-03 18:25:35 +0500

    Fix MealServlet

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -45,9 +45,6 @@ public class MealServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.info("getAll");
-        request.setAttribute("meals", MealsUtil.getTos(MealsUtil.MEALS, MealsUtil.DEFAULT_CALORIES_PER_DAY));
-        request.getRequestDispatcher("/meals.jsp").forward(request, response);
         String action = request.getParameter("action");
 
         switch (action == null ? "all" : action) {
