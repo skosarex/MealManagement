c9411a461a07a6542e9d52f68d5352eefbd2f6f3
Renat Skosarev <skosarrrev@gmail.com>
2022-11-27 06:29:12 +0500

    HW 1.2: test style.css

---
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -0,0 +1,20 @@
+package ru.javawebinar.topjava.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+
+public class ResourceControllerTest extends AbstractControllerTest {
+    @Test
+    public void testStyles() throws Exception {
+        mockMvc.perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.valueOf("text/css")));
+    }
+}
